9d14e73a2176f8b35b1f672c314c594bdfddd8d1
Oleg Gorecan <rvo@btinternet.com>
2017-11-22 16:46:19 +0000

    Add project files.

---
+++ Week1ThurLabTask4 - First Look at Variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1ThurLabTask4___First_Look_at_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number1;
+            decimal number2;
+            decimal userValue;
+            decimal result;
+
+            number1 = 11;
+            number2 = 7;
+
+            Console.Write("Please enter a value: ");
+            userValue = decimal.Parse(Console.ReadLine());
+
+            result = (number1 * number2) / userValue;
+
+            Console.WriteLine("The result of {0:N} times {1:N} divided by {2:N} is {3:N}", number1, number2, userValue, result);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
